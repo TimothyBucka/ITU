f91576366129323f4e0b393fd1e274a182115c08
TimothyBucka <timotej.bucka@gmail.com>
2023-12-14 20:37:54 +0100

    Mobile friendly account page and nav changes

--- nutripluscal/resources/js/app.vue
+++ nutripluscal/resources/js/app.vue
@@ -1,40 +1,47 @@
 <template>
-    <nav class="navbar navbar-expand-md bg-body-tertiary" data-bs-theme="dark">
-        <div class="container-fluid">
-            <router-link class="navbar-brand" to="/">NutriPlusCal</router-link>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <router-link class="nav-link" aria-current="page" to="/">Home</router-link> <!-- Link defined in router -->
-                    </li>
+    <OnClickOutside @trigger="closeNav">
+        <nav class="navbar navbar-expand-md bg-body-tertiary" data-bs-theme="dark">
+            <div class="container-fluid">
+                <router-link class="navbar-brand" to="/">NutriPlusCal</router-link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <router-link @click="closeNav()" class="nav-link" aria-current="page" to="/">Home</router-link>
+                        </li>
 
-                    <li class="nav-item">
-                        <router-link class="nav-link" aria-current="page" to="/restaurants">Restaurants</router-link>
-                    </li>
+                        <li class="nav-item">
+                            <router-link @click="closeNav()" class="nav-link" aria-current="page"
+                                to="/restaurants">Restaurants</router-link>
+                        </li>
 
-                    <li class="nav-item">
-                        <router-link class="nav-link" aria-current="page" to="/y_meals">Yours meals</router-link>
-                    </li>
+                        <li class="nav-item">
+                            <router-link @click="closeNav()" class="nav-link" aria-current="page" to="/y_meals">Yours
+                                meals</router-link>
+                        </li>
 
-                    <li class="nav-item">
-                        <router-link class="nav-link" aria-current="page" to="/calendar">Calendar</router-link>
-                    </li>
+                        <li class="nav-item">
+                            <router-link @click="closeNav()" class="nav-link" aria-current="page"
+                                to="/calendar">Calendar</router-link>
+                        </li>
 
-                    <li class="nav-item">
-                        <router-link class="nav-link" aria-current="page" to="/account">Account</router-link>
-                    </li>
+                        <li class="nav-item">
+                            <router-link @click="closeNav()" class="nav-link" aria-current="page"
+                                to="/account">Account</router-link>
+                        </li>
 
-                </ul>
+                    </ul>
+                </div>
             </div>
-        </div>   
-    </nav>
-    <main class="container-md">
-        <router-view></router-view> <!-- this will render the active page  -->
-    </main>
+        </nav>
+    </OnClickOutside>
 
+    <main class="container-md">
+        <router-view :key="$route.fullPath"></router-view> <!-- this will render the active page  -->
+    </main>
 </template>
 
 <script>
@@ -44,5 +51,11 @@ export default {
     components: {
         RouterLink
     },
+
+    methods: {
+        closeNav() {
+            $('.navbar-collapse').collapse('hide');
+        }
+    }
 }
 </script>
